0dedf82475eb982cade799f61b788d43ee76cabd
ADMIN <ADMIN@LAB1-MAY07>
2024-02-26 08:26:49 +0700

    Add project files.

---
+++ baimoi/baimoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "chuong trinh tinh tong\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << so2 << "=" << tong << endl;
+	return 0;
+}
+
+
+
